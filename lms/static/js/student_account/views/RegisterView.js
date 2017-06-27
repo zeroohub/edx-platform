8dc880868a7037d8a9341d35e2edfb1d211129f3
Uman Shahzad <uman@opencraft.com>
2017-06-27 06:58:04 +0500

    Implement client-side registration form validation.
    Input forms that need validation will have AJAX requests
    performed to get validation decisions live.
    
    Information is conveyed on blur for both errors and successes.

--- lms/static/js/student_account/views/RegisterView.js
+++ lms/static/js/student_account/views/RegisterView.js
@@ -10,23 +10,21 @@
         function($, _, gettext, FormView, formStatusTpl) {
             return FormView.extend({
                 el: '#register-form',
-
                 tpl: '#register-tpl',
-
                 events: {
                     'click .js-register': 'submitForm',
-                    'click .login-provider': 'thirdPartyAuth'
+                    'click .login-provider': 'thirdPartyAuth',
+                    'blur #register-email': 'liveValidate',
+                    'blur #register-username': 'liveValidate',
+                    'blur #register-password': 'liveValidate',
+                    'focus input[required]': 'renderRequiredMessage' // the message should be hidden, but turned on by this.
                 },
-
                 formType: 'register',
-
                 formStatusTpl: formStatusTpl,
-
                 authWarningJsHook: 'js-auth-warning',
-
                 defaultFormErrorsTitle: gettext('We couldn\'t create your account.'),
-
                 submitButton: '.js-register',
+                requiredStr: gettext('(required)'),
 
                 preRender: function(data) {
                     this.providers = data.thirdPartyAuth.providers || [];
@@ -40,6 +38,7 @@
                     this.hideAuthWarnings = data.hideAuthWarnings;
 
                     this.listenTo(this.model, 'sync', this.saveSuccess);
+                    this.listenTo(this.model, 'validation', this.renderLiveValidations);
                 },
 
                 render: function(html) {
@@ -75,6 +74,67 @@
                     }
 
                     return this;
+                },
+
+                renderLiveValidations: function(target, decisions) {
+                    // TODO: Clean out the mess below?
+                    var $el = $(target),
+                        $label = this.$form.find('label[for=' + $el.attr('id') + ']'),
+                        $icon = $('#' + $el.attr('id') + '-validation-icon'),
+                        $errorTip = $('#' + $el.attr('id') + '-validation-error'),
+                        error = decisions.validation_decisions[$el.attr('name')],
+                        errorId = $el.attr('id') + '-validation-error-container';
+
+                    if (error) {
+                        this.renderLiveValidationError($el, $label, $icon, $errorTip, error);
+
+                        // Update the error message in the container.
+                        this.cleanErrorMessage(errorId);
+                        this.errors.push('<li id="' + errorId +'">' + error + '</li>');
+                        this.renderErrors(this.defaultFormErrorsTitle, this.errors);
+                    } else {
+                        this.renderLiveValidationSuccess($el, $label, $icon, $errorTip);
+
+                        // If an error for this field exists in the container, erase it.
+                        if ($('#' + errorId).length) {
+                            this.cleanErrorMessage(errorId);
+                            this.renderErrors(this.defaultFormErrorsTitle, this.errors);
+                        }
+                    }
+                },
+
+                renderLiveValidationError: function($el, $label, $icon, $tip, error) {
+                    $el.removeClass('success').addClass('error');
+                    $label.removeClass('success').addClass('error');
+                    $icon.removeClass('fa-check success').addClass('fa-times error');
+                    $tip.text(error);
+                },
+
+                renderLiveValidationSuccess: function($el, $label, $icon, $tip) {
+                    $el.removeClass('error').addClass('success');
+                    $label.removeClass('error').addClass('success');
+                    $icon.removeClass('fa-times error').addClass('fa-check success');
+                    $tip.text('');
+
+                    // Hide success indicators after some time.
+                    var self = this;
+                    setTimeout(function() { self.cleanLiveValidationSuccess($el, $label, $icon) },
+                        10000
+                    );
+                },
+
+                cleanLiveValidationSuccess: function($el, $label, $icon) {
+                    $el.removeClass('success');
+                    $label.removeClass('success');
+                    $icon.removeClass('fa-check success');
+                },
+
+                cleanErrorMessage: function(id) {
+                    for (var i = 0; i < this.errors.length; ++i) {
+                        if (this.errors[i].includes(id)) {
+                            this.errors.splice(i, 1);
+                        }
+                    }
                 },
 
                 thirdPartyAuth: function(event) {
@@ -159,6 +219,18 @@
                     }
 
                     return obj;
+                },
+
+                liveValidate: function(event) {
+                    var url = '/api/user/v1/validation/registration',
+                        dataType = 'json',
+                        data = {
+                            username: $('#register-username').val(),
+                            email: $('#register-email').val(),
+                            password: $('#register-password').val()
+                        },
+                        method = 'POST';
+                    FormView.prototype.liveValidate(event, url, dataType, data, method, this.model);
                 }
             });
         });
